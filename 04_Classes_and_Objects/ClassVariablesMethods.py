df6d866f3f673c5cf2ab42c7b3ef0a826d757a0f
Irfan Khan Mohammed <mr.irfankhan196@gmail.com>
2025-12-28 22:51:35 -0600

    Added code for class variables and methods

---
+++ 04_Classes_and_Objects/ClassVariablesMethods.py
@@ -0,0 +1,14 @@
+
+class ToyotaCar:
+    
+    car = "Toyota Corolla"
+
+    def buildcar():
+        print("Toyota Corolla 2025 is build.")
+
+
+
+if __name__ == "__main__":
+
+    print(ToyotaCar.car)
+    ToyotaCar.buildcar()
