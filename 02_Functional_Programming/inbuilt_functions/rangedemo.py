875207b5bd84384aada976a068856103d006792e
Irfan Khan Mohammed <mr.irfankhan196@gmail.com>
2026-01-23 00:55:57 -0600

    feat: added inbuild_functions programs

---
+++ 02_Functional_Programming/inbuilt_functions/rangedemo.py
@@ -0,0 +1,18 @@
+
+lst = list(range(10))
+
+# or
+
+lst = list(range(1, 11))
+
+print(lst)
+
+
+stt = set(range(1, 10))
+
+print(stt)
+
+# third parameter acts as an incrementor
+stt2 = set(range(0, 20, 2))
+
+print(stt2)
